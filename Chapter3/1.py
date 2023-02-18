64f5d9f47cf74dd942de1927e68c5d88447f8da7
LlamaUnicorn <holidaynight@gmail.com>
2023-02-18 18:43:38 +0300

    first commit

---
+++ Chapter3/1.py
@@ -0,0 +1,13 @@
+print('hello')
+var = 1
+
+
+class My():
+    pass
+
+print('world')
+
+import time
+start_time = time.time()
+sum(range(100000))
+print('--- %s seconds ---' % (time.time() - start_time))
